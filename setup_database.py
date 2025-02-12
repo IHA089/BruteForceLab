6f194b7e1fcb505b00afc4deb48b185df9749be6
IHA089 <67224300+Rohitash465@users.noreply.github.com>
2025-02-12 17:58:47 +0530

    Add files via upload

---
+++ setup_database.py
@@ -0,0 +1,25 @@
+import sqlite3
+import random
+import hashlib
+
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    gmail TEXT NOT NULL,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+)
+''')
+
+numb = random.randint(100, 999)
+passw = "admin@"+str(numb)
+passw_hash = hashlib.md5(passw.encode()).hexdigest()
+query = "INSERT INTO users (gmail, username, password) VALUES ('admin@iha089.org', 'admin', '"+passw_hash+"')"
+
+cursor.execute(query)
+
+conn.commit()
+conn.close()
