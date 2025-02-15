80108605d6e664a130fe34cd6e5be0554fd04e2f
IHA089 <67224300+Rohitash465@users.noreply.github.com>
2025-02-15 13:34:18 +0530

    Update setup_database.py

--- setup_database.py
+++ setup_database.py
@@ -2,24 +2,26 @@ import sqlite3
 import random
 import hashlib
 
-conn = sqlite3.connect('users.db')
-cursor = conn.cursor()
 
-cursor.execute('''
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    gmail TEXT NOT NULL,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
-)
-''')
+def create_database():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
 
-numb = random.randint(100, 999)
-passw = "admin@"+str(numb)
-passw_hash = hashlib.md5(passw.encode()).hexdigest()
-query = "INSERT INTO users (gmail, username, password) VALUES ('admin@iha089.org', 'admin', '"+passw_hash+"')"
+    cursor.execute('''
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        gmail TEXT NOT NULL,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL
+    )
+    ''')
 
-cursor.execute(query)
+    numb = random.randint(100, 999)
+    passw = "admin@"+str(numb)
+    passw_hash = hashlib.md5(passw.encode()).hexdigest()
+    query = "INSERT INTO users (gmail, username, password) VALUES ('admin@iha089.org', 'admin', '"+passw_hash+"')"
 
-conn.commit()
-conn.close()
+    cursor.execute(query)
+
+    conn.commit()
+    conn.close()
